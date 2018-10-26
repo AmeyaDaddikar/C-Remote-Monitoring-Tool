dda2b1489c625b81b1e2ea1eab3626d0534cde25
Ameya Daddikar <ameyadaddikar@gmail.com>
2018-10-26 14:41:59 +0530

    Added video functionality (Buggy)

--- RaspPI_Code/video.py
+++ RaspPI_Code/video.py
@@ -14,22 +14,19 @@ class VideoWindow(Toplevel):
     #title = "" #this would block the method to override the current title
     message = ""
 
-    def __init__(self, master, nid, title, message,sc):
+    def __init__(self, master,sc):
       Toplevel.__init__(self,master)
-      self.nid = nid 
-      self.title(title) #since toplevel widgets define a method called title you can't store it as an attribute
-      self.message = message
       self.display_note_gui() #maybe just leave that code part of the __init__?
       self.socket_conn = sc
 
     def display_video_gui(self): 
       '''Tkinter to create a note gui window with parameters '''    
       #no window, just self
-      self.geometry("200x200")
+      self.geometry("600x600")
       self.configure(background="#BAD0EF")
       #pass self as the parent to all the child widgets instead of window
-      title = Entry(self,relief=FLAT, bg="#BAD0EF", bd=0)
-      title.pack(side=TOP)
+    #   title = Entry(self,relief=FLAT, bg="#BAD0EF", bd=0)
+    #   title.pack(side=TOP)
       
       print(display_video_data(self.socket_conn))
       #self.mainloop() #leave this to the root window
